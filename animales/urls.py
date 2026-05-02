be3c08d091e0735d8872c7a79143c0209b9a275e
maario301 <mario.monzon.peso@gmail.com>
2026-05-02 15:18:01 +0200

    Carpetas animales+ api-venenos actualizadas

--- animales/urls.py
+++ animales/urls.py
@@ -1,24 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import AnimalViewSet, ConsultaViewSet
-
-router = DefaultRouter()
-router.register(r'animales', AnimalViewSet)
-router.register(r'consultas', ConsultaViewSet)
-
-from django.urls import path
-from . import views
-# Importamos las vistas de la librería JWT
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+# 1. El router se encarga de la ruta 'animales/' automáticamente usando las clases
+router = DefaultRouter()
+router.register(r'animales', AnimalViewSet)
+router.register(r'consultas', ConsultaViewSet)
+
 urlpatterns = [
-    # Ruta para ver los animales
-    path('animales/', views.lista_animales, name='lista_animales'),
+    # Esto incluye automáticamente la ruta 'animales/' que tenías antes,
+    # pero conectándola a la clase AnimalViewSet que es la que existe.
+    path('', include(router.urls)), 
     
-    # Rutas para el Login (JWT)
+    # Rutas para el Login (JWT) que necesitas para Android
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
