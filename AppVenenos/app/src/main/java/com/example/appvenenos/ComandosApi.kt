c40f21d62b2c5b62b4851683a54d2dd4f9f5a0a9
maario301 <mario.monzon.peso@gmail.com>
2026-05-08 17:21:40 +0200

    Actualización: Seguridad JWT implementada y rutas corregidas

--- AppVenenos/app/src/main/java/com/example/appvenenos/ComandosApi.kt
+++ AppVenenos/app/src/main/java/com/example/appvenenos/ComandosApi.kt
@@ -7,11 +7,9 @@ import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ComandosApi {
-    // Para el login
-    @POST("api/token/")
+    @POST("api/token/") // Asegúrate de que termina en /
     fun login(@Body credentials: Map<String, String>): Call<TokenResponse>
 
-    // Para los animales (ahora con Header de autorización)
-    @GET("api/animales/")
+    @GET("api/animales/") // Asegúrate de que termina en /
     fun getAnimales(@Header("Authorization") token: String): Call<List<Animal>>
 }
--- api_venenos/urls.py
+++ api_venenos/urls.py
@@ -4,8 +4,10 @@ from django.urls import path, include
 urlpatterns = [
     # La ruta del panel de control que ya tienes
     path('admin/', admin.site.urls),
-    
-    # Esta es la línea maestra: redirige todo lo que sea 'api/...' 
-    # al archivo urls.py de tu carpeta 'animales'
-    path('api/', include('animales.urls')), 
+
+    # Esta es la línea maestra para los animales
+    path('api/', include('animales.urls')),
+
+    # AÑADE ESTA LÍNEA AQUÍ ABAJO:
+    path('api-auth/', include('rest_framework.urls')),
 ]
