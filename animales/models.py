78287fac3ebae3c40f6956b44c456558474caee0
maario301 <mario.monzon.peso@gmail.com>
2026-05-12 13:14:55 +0200

    Actualizados modelos Animal y Consulta para historial y mapa

--- animales/models.py
+++ animales/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User # Importamos los usuarios de Django
 
 class Animal(models.Model):
     nombre_comun = models.CharField(max_length=100)
-    nombre_cientifico = models.CharField(max_length=100) # Ya sin el italic
+    nombre_cientifico = models.CharField(max_length=100, unique=True)
     descripcion = models.TextField()
     
     toxicidad = models.CharField(max_length=50) 
@@ -15,10 +16,15 @@ class Animal(models.Model):
         return self.nombre_comun
 
 class Consulta(models.Model):
-    firebase_uid = models.CharField(max_length=128)
+    # Relacionamos la consulta directamente con el usuario logueado en la App
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) 
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
     fecha = models.DateTimeField(auto_now_add=True)
     probabilidad = models.FloatField(default=0.0)
+    
+    # Campos vitales para el Paso 5: El Mapa
+    latitud = models.FloatField(null=True, blank=True)
+    longitud = models.FloatField(null=True, blank=True)
 
     def __str__(self):
-        return f"Consulta de {self.animal.nombre_comun} - {self.fecha}"
+        return f"{self.usuario.username} vio un {self.animal.nombre_comun} el {self.fecha}"
