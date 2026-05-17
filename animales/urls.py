b5cc898e5f3b99ea5ba85056694ed27e514e9155
maario301 <mario.monzon.peso@gmail.com>
2026-05-18 00:41:28 +0200

    Marcador 8

--- animales/urls.py
+++ animales/urls.py
@@ -1,28 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AnimalViewSet, ConsultaViewSet, RegistroUsuarioView # 1. Importa la nueva vista
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import AnimalViewSet, ConsultaViewSet, RegistroUsuarioView, GuardarAvistamientoView, ListarAvistamientosView
-
 
 router = DefaultRouter()
 router.register(r'animales', AnimalViewSet)
 router.register(r'consultas', ConsultaViewSet)
 
 urlpatterns = [
-    # 1. LA RUTA DE REGISTRO DEBE IR PRIMERO Y SIN "api/" 
-    # (Porque el "api/" ya se lo pone el archivo principal)
     path('register/', RegistroUsuarioView.as_view(), name='registro_usuario'),
-
-    # 2. Las rutas del Token
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('avistamientos/', ListarAvistamientosView.as_view(), name='listar_avistamientos'),
     path('avistamientos/guardar/', GuardarAvistamientoView.as_view(), name='guardar_avistamiento'),
-
-    # 3. El router al final
     path('', include(router.urls)),
 ]
