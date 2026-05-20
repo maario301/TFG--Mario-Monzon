5aa7b5f60df440df5fcc1c9b6e9f2773d1faa33d
maario301 <mario.monzon.peso@gmail.com>
2026-05-20 23:19:20 +0200

    Swagger 3

--- api_venenos/urls.py
+++ api_venenos/urls.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+# 1. AÑADE ESTOS IMPORTS PARA SWAGGER
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# 2. CONFIGURA EL SCHEMA_VIEW AQUÍ
+schema_view = get_schema_view(
+   openapi.Info(
+      title="VenoMap API",
+      default_version='v1',
+      description="Documentación de la API de VenoMap",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
